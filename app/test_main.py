9bc7d6bcd01d9a5fa134ec1d2954c593f5e9fe63
Svetlana <gladkovansk@gmail.com>
2026-05-12 22:06:22 +0200

    Add tests and changed a build.yaml file

---
+++ app/test_main.py
@@ -0,0 +1,22 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Welcome to my Research Project",
+        "status": "Ready"}
+    
+def test_healt_check():
+    response = client.get('/health')
+    assert response.status_code == 200
+    assert response.json()["status"] == "UP" 
+
+def test_system_metrics():
+    response = client.get("/system")
+    assert response.status_code == 200
+    assert "cpu_load" in response.json()
+    assert "memory_load" in response.json()
