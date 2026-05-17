5e3e7b6cc160f7aeab00fc6fd4404de419ffaaec
Svetlana <gladkovansk@gmail.com>
2026-05-17 17:51:11 +0200

    changes because of lint

--- app/test_main.py
+++ app/test_main.py
@@ -3,17 +3,20 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {
         "message": "Welcome to my Research Project",
         "status": "Ready"}
-    
+
+
 def test_healt_check():
     response = client.get('/health')
     assert response.status_code == 200
-    assert response.json()["status"] == "UP" 
+    assert response.json()["status"] == "UP"
+
 
 def test_system_metrics():
     response = client.get("/system")
